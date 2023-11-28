4ae0eecd455db0952e38940191b042196fc2bd97
Tyler Prill <typrill@duosecurity.com>
2023-11-28 13:42:57 -0500

    Working with remote app up

--- shared_assets_directory/src/SectionFromRemote/index.tsx
+++ shared_assets_directory/src/SectionFromRemote/index.tsx
@@ -1,6 +1,7 @@
 const SectionFromRemote = (): JSX.Element => (
   <div id={"microfrontend-react-typescript"}>
-    This particular div is being generated from a React + Typescript component.
+    This particular div is being generated from a React + Typescript component
+    TESTING.
   </div>
 );
 
