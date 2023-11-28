8fca51c46519387bbcf8fdc2316731376a26ea74
Tyler Prill <typrill@duosecurity.com>
2023-11-28 14:11:38 -0500

    clean up

--- shared_assets_directory/lib/esm/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const SectionFromRemote: () => JSX.Element;
-export default SectionFromRemote;
--- shared_assets_directory/src/SectionFromRemote/index.tsx
+++
@@ -1,8 +0,0 @@
-const SectionFromRemote = (): JSX.Element => (
-  <div id={"microfrontend-react-typescript"}>
-    This particular div is being generated from a React + Typescript component
-    TESTING.
-  </div>
-);
-
-export default SectionFromRemote;
