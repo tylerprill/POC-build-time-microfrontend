94b4f0cba478c64a515b4998adb0e4d78cdc4237
Tyler Prill <typrill@duosecurity.com>
2023-11-28 15:06:47 -0500

    fix remote

--- remote-application/shared_assets_directory/src/index.tsx
+++
@@ -1,8 +0,0 @@
-const SectionFromRemote = (): JSX.Element => (
-  <div id={"microfrontend-react-typescript"}>
-    This particular div is being generated from a Microfrontend:
-    SectionFromRemote
-  </div>
-);
-
-export default SectionFromRemote;
